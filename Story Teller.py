8a54aeea53d985c3133ea539b354c12261e9d95d
duongd08 <40089386+duongd08@users.noreply.github.com>
2018-06-08 23:53:47 -0700

    Create Story Teller.py

---
+++ Story Teller.py
@@ -0,0 +1,8 @@
+noun = input("Enter a singular noun: ")
+adj = input("Enter an adjective: ")
+name = input("Enter a name: ")
+animal = input("Enter an animal: ")
+action = input("Enter an action: ")
+pastaction = input("Enter the action above in past tense: ")
+
+print('Once upon a time there was a %s %s named %s. \n One day a %s came over and said \" I am going to %s you\"  and the %s %s %s. The %s live a happy life. \a' %(adj, noun, name, animal, action, animal, pastaction,  name, animal))
